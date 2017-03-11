158eaebcbdcec0e44ed9b87ca61100674b00a835
Chirag Gupta <chirag@walkover.in>
2017-03-11 12:15:24 +0530

    +000 | + Chirag |  added profile system (Satble)

--- src/test/java/com/ApiUtils/UrlConfig.java
+++ src/test/java/com/ApiUtils/UrlConfig.java
@@ -3,7 +3,7 @@ package com.ApiUtils;
 import org.aeonbits.owner.Config;
 import org.aeonbits.owner.Config.Sources;
 
-@Sources({"classpath:config/URL.#{systemProperties['apitest.env']}.properties"})
+@Sources({"classpath:config/URL.${api.environment}.properties"})
 public interface UrlConfig extends  Config{
 
     @Key("url")

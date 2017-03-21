60111a801477ccec8daac18d5bfd9b94f2d1fcfa
Chirag Gupta <chirag@walkover.in>
2017-03-21 19:14:01 +0530

    +000 | + Chirag | Added Invoice API changes  (Stable)

--- src/test/java/com/api/Smoke/InvoiceAPI.java
+++ src/test/java/com/api/Smoke/InvoiceAPI.java
@@ -3,33 +3,38 @@ package com.api.Smoke;
 
 import com.ApiUtils.ApiManager;
 import com.ApiUtils.HelperMethods;
+import com.ApiUtils.JsonUtil;
 import com.ApiUtils.SmartResponse;
 import com.Config.UrlConfig;
 import com.model.Invoice;
 import com.model.ManageHeaders;
-import com.model.ManageURL;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.aeonbits.owner.ConfigFactory.create;
+import static org.aeonbits.owner.ConfigFactory.*;
 
 public class InvoiceAPI {
 
 
     ManageHeaders header = new ManageHeaders();
     ApiManager apiManager = new ApiManager();
-    ManageURL baseURL = new ManageURL();
     UrlConfig config = create(UrlConfig.class);
 
     @Test
     public void createInvoice() throws  Exception{
         HelperMethods.setAnsiGreen("Started :- Create Invoice");
-        String uniqueName[]={LedgerAPI.ledger_UniqueName};
-        //System.out.println(uniqueName + "in invoice ");
-        Invoice invoice = new Invoice(uniqueName);
+        List<String> uniqueNames = new ArrayList<>();
+        uniqueNames.add(LedgerAPI.ledger_UniqueName);
+        System.out.println(uniqueNames + "in invoice ");
+        Invoice invoice = new Invoice(uniqueNames);
+        String body = JsonUtil.toJsonAsString(invoice);
         System.out.println(invoice);
-        SmartResponse resp = apiManager.postAPI_with_Assert_Statuscode(config.createInvoice(), invoice);
+        SmartResponse resp = apiManager.postAPI_with_Assert_Statuscode(config.createInvoice(), body);
         System.out.println(resp.getJson());
+        for ( String data : uniqueNames) {
+            System.out.println(data);
+        }
     }
 }
--- src/test/java/com/model/Invoice.java
+++ src/test/java/com/model/Invoice.java
@@ -4,16 +4,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class Invoice {
 
-    private String uniqueNames[];
+    private List<String> uniqueNames;
 
-    public Invoice(String uniqueNames[]) {
-        this.uniqueNames = new String[uniqueNames.length];
-        for (int i = 0; i < uniqueNames.length; i++) {
-            this.uniqueNames[i] = uniqueNames[i];
-        }
+//    public Invoice(String uniqueNames[]) {
+//        this.uniqueNames = new String[uniqueNames.length];
+//        for (int i = 0; i < uniqueNames.length; i++) {
+//            this.uniqueNames[i] = uniqueNames[i];
+//        }
+//    }
+    public Invoice(List<String> uniqueNames) {
+        this.uniqueNames = uniqueNames;
     }
 }

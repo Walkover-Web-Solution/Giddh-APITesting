1378590b2d30aa5542113af4abccd487eac959b6
Chirag Gupta <chirag@walkover.in>
2017-04-14 17:04:44 +0530

    +000 | + Chirag | Added search with assert in flatten grup with account list (Stable)

--- src/test/java/com/api/smoke/FlattenAPI.java
+++ src/test/java/com/api/smoke/FlattenAPI.java
@@ -22,15 +22,31 @@ public class FlattenAPI {
         /**
          * Main test and api call initiated
          */
-
-        SmartResponse response = methodManager.getAPI_With_Params(null, null, config.get_Flatten_Group_With_Accounts(), null, null, null, false);
+        SmartResponse response = methodManager.getAPI_With_Params(null, null, config.get_Flatten_Group_With_Accounts(), null, null, null, true);
         System.out.println(response.getJson());
         System.out.println(response.getStatusCode());
         String json = response.getJson();
         JsonPath jp = new JsonPath(json);
-        assertEquals(jp.get("body.results[2].groupUniqueName"), "cash");
+        //assertEquals(jp.get("body.results[2].groupUniqueName"), "cash");
+        assertEquals(jp.get("body.page"), 1);
+        assertEquals(jp.get("body.count"), 5);
+        assertEquals(jp.get("body.totalPages"), 2);
+        assertEquals(jp.get("body.totalItems"), 7);
+    }
+
+    @Test
+    public void flatten_Group_with_Accounts_with_Cash_Search(){
+
+        HelperMethods.setAnsiGreen("Started :- Get flatten group-with-accounts API");
+
+        /**
+         * Main test and api call initiated
+         */
+        SmartResponse response = methodManager.getAPI_With_Params(null, null, config.get_Flatten_Group_With_Accounts(), null, null, "cash", true);
+        String json = response.getJson();
+        JsonPath jp = new JsonPath(json);
+        assertEquals(jp.get("body.results[0].groupUniqueName"), "cash");
         assertEquals(jp.get("body.page"), 1);
         assertEquals(jp.get("body.count"), 5);
     }
-
 }

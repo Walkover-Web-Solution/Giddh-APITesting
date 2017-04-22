a62d26d1061195f0e8330d19b444f2da6c28f471
Chirag Gupta <chirag@walkover.in>
2017-04-22 16:00:51 +0530

    +000 | + Chirag | create Stock changes* (Stable)

--- src/test/java/com/model/Stock/StockAccountDetails.java
+++ src/test/java/com/model/Stock/StockAccountDetails.java
@@ -1,9 +1,17 @@
 package com.model.Stock;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StockAccountDetails {
 
     private String accountUniqueName;
+    private List<UnitRateInput> UnitRateInput = new ArrayList<>();
 
+    public StockAccountDetails(String accountUniqueName, List<UnitRateInput> UnitRateInput ){
+        this.accountUniqueName=accountUniqueName;
+        this.UnitRateInput=UnitRateInput;
+    }
 
 }
--- src/test/java/com/model/Stock/UnitRateInput.java
+++ src/test/java/com/model/Stock/UnitRateInput.java
@@ -3,12 +3,12 @@ package com.model.Stock;
 
 import java.math.BigDecimal;
 
-public class StockUnitCode {
+public class UnitRateInput {
 
     private BigDecimal rate;
     private String stockUnitCode;
 
-    public StockUnitCode (BigDecimal rate, String stockUnitCode){
+    public UnitRateInput(BigDecimal rate, String stockUnitCode){
         this.rate=rate;
         this.stockUnitCode=stockUnitCode;
     }
